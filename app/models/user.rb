d4007f1bc1e4f394f12a0b6923491865ec5da108
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-08 18:03:52 +0900

    userモデル実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
+  before_validation { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
