bdb3ac56888a38442212a5e0f21e3849c3715a74
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-08 18:22:15 +0900

    ログイン、ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  def authenticate_user
+    if @current_user == nil
+
+      redirect_to new_session_path
+    end
+  end
+  def check_user
+    if current_user.id != @feed.user.id
+      flash[:notice] = "権限がありません"
+      redirect_to feeds_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
