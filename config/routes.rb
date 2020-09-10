16248f271eaf5debc418ba02370d9c397eed3dc2
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-10 18:12:31 +0900

    お気に入りした投稿を抽出できるようルート定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  get '/users/:id/favorites', to: 'users#favorites'
   resources :pictures do
     collection do
       post :confirm
