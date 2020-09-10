a00ad307a5bcc4c7fae2e24572ce60c9f243b1c0
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-10 10:30:33 +0900

    pictureのroute定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
